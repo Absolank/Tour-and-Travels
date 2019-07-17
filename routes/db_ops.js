a40d88fc900097395c5199c6340b9f1c62281179
Absolank <ankitkripal@gmail.com>
2019-07-17 05:52:01 +0530

    Final

--- routes/db_ops.js
+++ routes/db_ops.js
@@ -52,7 +52,6 @@ exports.deleteLocation = (req, res) => {
 
 };
 
-
 exports.addCountry = (req, res) => {
     const countryName = req.body.country_name;
     const insertCountry = 'insert into Country (Name) values (?)';
@@ -67,6 +66,7 @@ exports.addCountry = (req, res) => {
         }
     });
 };
+
 exports.addCity = (req, res) => {
     const countryId = req.body.country_id;
     const name = req.body.city_name;
@@ -83,6 +83,7 @@ exports.addCity = (req, res) => {
         }
     });
 };
+
 exports.addCityPhoto = (req, res) => {
     const cityId = req.body.City_id;
     const url = req.body.place_url;
@@ -100,6 +101,7 @@ exports.addCityPhoto = (req, res) => {
         });
     });
 };
+
 exports.addPlace = (req, res) => {
     const cityId = req.body.city_id;
     const name = req.body.city_name;
@@ -118,6 +120,7 @@ exports.addPlace = (req, res) => {
         }
     });
 };
+
 exports.addPlacePhoto = (req, res) => {
     const placeId = req.body.place_id;
     const url = req.body.place_url;
@@ -147,22 +150,26 @@ exports.addUser = (req, res) => {
     console.log(username + " " + firstname + " " + lastname + " " + email + " " + password);
     if (username == null || firstname == null || lastname == null || email == null || password == null) {
         res.send({
-            'error': '400',
-            'message': 'empty fields'
+            code: 400,
+            message: 'empty fields'
         });
         return;
     }
     const queryUrl = 'insert into Users (Username, FirstName, LastName, Email, Password) values (?, ?, ?, ?, PASSWORD(?))';
 
     connection.query(queryUrl, [username, firstname, lastname, email, password], (err, results, fields) => {
-        if (err) {
-            console.log("error ocurred", err.code);
-            res.send({
-                'err': [err.errno, err.code]
+        
+        if(err){
+            res.json({
+                code: 400,
+                message: 'failed to create account'
             });
-        } else {
-            res.json(results);
+            return;
         }
+        res.json({
+            code: 200,
+            message: 'successfully created account'   
+        });
     });
 };
 
@@ -249,15 +256,38 @@ exports.searchUser = (req, res) => {
 };
 
 exports.addFlight = (req, res) => {
-    const tourId = req.body.tourId;
+    const tourId = req.body.tour_id;
     const cost = req.body.cost;
-    const startDate = req.body.date;
-    const startTime = req.body.startTime;
-    const travelTime = req.body.travelTime;
-    const queryInsertFlight = 'insert into Flight (TourID, Cost, StartDate, StartTime, TravelTime) values (?, ?, ?, ?, ?) ';
-    connection.query(queryInsertFlight, [tourId, cost, startDate, startTime, travelTime], (err, results, fields) => {
+    const departure = req.body.departure;
+    const travelTime = req.body.travel_time;
+    const queryInsertFlight = 'insert into Flight (TourID, Cost, Departure, TravelTime) values (?, ?, ?, ?) ';
+    connection.query(queryInsertFlight, [tourId, cost, departure, travelTime], (err, results, fields) => {
+        if(err){
+            res.json({
+                code: 400,
+                message: 'failed to add flight'
+            });
+            return;
+        }
         res.json({
-            err,
+            code: 200,
+            message: 'successfully added flight'   
+        });
+    });
+};
+
+exports.getFlights = (req, res) =>{
+    const tourId = req.body.tour_id;
+    const query = 'select ID, Cost, Departure, TravelTime from Flight where TourID = ?';
+    connection.query(query, [tourId], (err, results, fields)=>{
+        if(err){
+            res.json({
+                code: 400,
+                message: 'failed to get flights'
+            });
+            return;
+        }
+        res.json({
             results
         });
     });
@@ -300,12 +330,19 @@ exports.addHotel = (req, res) => {
     const place_id = req.body.place_id;
     const perPersonCost = req.body.cost;
     const star = req.body.star;
-    const insertHotel = 'insert into Hotel (Name, PlaceID, PerPersonCost, Star) values (?, ?, ?)';
-    connection.query(insertHotel, [hotelName, place_id, perPersonCost, star], (err, records, fields) => {
+    const url = req.body.image_url;
+    const insertHotel = 'insert into Hotel (Name, ImageUrl, PlaceID, PerPersonCost, Star) values (?, ?, ?, ?)';
+    connection.query(insertHotel, [hotelName, Iurl, place_id, perPersonCost, star], (err, records, fields) => {
+        if(err){
+            res.json({
+                code: 400,
+                message: 'failed to add hotel'
+            });
+            return;
+        }
         res.json({
-            err,
-            records,
-            fields
+            code: 200,
+            message: 'successfully added hotel'   
         });
     });
 };
@@ -315,10 +352,16 @@ exports.addHotelPhoto = (req, res) => {
     const image_url = req.body.image_url;
     const insertPhoto = 'insert into HotelImage (HotelID, Url) values (?, ?)';
     connection.query(insertPhoto, [hotelId, image_url], (err, records, fields) => {
+        if(err){
+            res.json({
+                code: 400,
+                message: 'failed to add hotel image'
+            });
+            return;
+        }
         res.json({
-            err,
-            records,
-            fields
+            code: 200,
+            message: 'successfully added hotel image'   
         });
     });
 };
@@ -329,22 +372,28 @@ exports.addHotelReview = (req, res) => {
     const rating = req.body.rating;
     const insertHotelReview = 'insert into HotelReviews (HotelID, Review, Rating) values (?, ?, ?)';
     connection.query(insertHotelReview, [hotelID, review, rating], (err, records, fields) => {
+        if(err){
+            res.json({
+                code: 400,
+                message: 'failed to add hotel reviews'
+            });
+            return;
+        }
         res.json({
-            err,
-            records,
-            fields
+            code: 200,
+            message: 'successfully added hotel reviews'   
         });
     });
 };
 
-exports.getHotel = (req, res) => {
+exports.getHotels = (req, res) => {
     const cityId = req.body.city_id;
-    const query = "select HotelID, Name, PerPersonCost, Start," +
+    const query = "select HotelID, Name, ImageUrl, PerPersonCost, Star," +
      "Place.Name as PlaceName, City.Name as CityName, Country.Name as CountryName from Hotel"+
      "inner join Place on Place.CityID = City.ID"+
      "inner join Country on City.CountryID = Country.ID"+
      "where CityID = ?";
-    
+
     connection.query(query, [cityId], (err, results, fields)=>{
         if(err){
             res.json({
@@ -353,11 +402,152 @@ exports.getHotel = (req, res) => {
             });
         }
         res.json({
-            records
+            results
         });
     });
 };
 
+exports.bookHotel = (req, res) => {
+    const userId = req.body.user_id;
+    const hotelId = req.body.hotel_id;
+    const numPerson = req.body.num_person;
+    const debitCardNum = req.body.card_number;
+    const debitCVV = req.body.cvv;
+    const numDays = req.body.num_days;
+    const check_in_date = req.body.check_in_date;
+    const accountHolderName = req.body.account_holder_name;
+    const date = Date.now();
+    const transactionQuery = 'insert into Transaction (Amount, CardNumber, AccountHolderName, TransactionDate) values (?, ?, ?, ?);';
+    const hotelInvoice = 'insert into HotelInvoice(UserID, HotelID, CheckInDate, NumDays, NumPerson, TransactionID) values(?, ?, ?, ?, ?, LAST_INSERT_ID())';
+    const getPerPersonCost = 'select PerPersonCost from Hotel where ID = ?';
+    connection.beginTransaction((err)=>{
+        if(err){
+            throw err;
+        }
+        connection.query(getPerPersonCost, [hotelId], (err, results, fields) => {
+            if(err){
+                throw err;
+            }
+            const perPersonCost = results[0].PerPersonCost;
+            const amount = numPerson * perPersonCost;
+            connection.query(transactionQuery, [amount, debitCardNum, accountHolderName, date], (err, results, fields)=>{
+                if(err){
+                    connection.rollback(()=>{
+                        throw err;
+                    });
+                }
+                connection.query(hotelInvoice, [userId, hotelId, check_in_date, numDays, numPerson], (err, results, fields)=>{
+                    if(err){
+                        connection.rollback(()=>{
+                            throw err;
+                        });
+                    }
+                    connection.commit((err)=>{
+                        if(err){
+                            connection.rollback(()=>{
+                                throw err;
+                            });
+                        }
+                    });
+                });
+            });
+        });
+    });
+};
+
+exports.bookFlight = (req, res) => {
+    const userId = req.body.user_id;
+    const flightID = req.body.flight_id;
+    const numPerson = req.body.num_person;
+    const debitCardNum = req.body.card_number;
+    const debitCVV = req.body.cvv;
+    const accountHolderName = req.body.account_holder_name;
+    const date = Date.now();
+    const departureDate = req.body.departure_date;
+    const transactionQuery = 'insert into Transaction (Amount, CardNumber, AccountHolderName, TransactionDate) values (?, ?, ?, ?);';
+    const flightInvoice = 'insert into FlightInvoice(UserID, FlightID, DepartureDate, NumPerson, TransactionID) values(?, ?, ?, ?, LAST_INSERT_ID())';
+    const getPerPersonCost = 'select PerPersonCost from Flight where FlightID = ?';
+    connection.beginTransaction((err)=>{
+        if(err){
+            throw err;
+        }
+        connection.query(getPerPersonCost, [flightID], (err, results, fields) => {
+            if(err){
+                throw err;
+            }
+            const perPersonCost = results[0].PerPersonCost;
+            const amount = numPerson * perPersonCost;
+            connection.query(transactionQuery, [amount, debitCardNum, accountHolderName, date], (err, results, fields)=>{
+                if(err){
+                    connection.rollback(()=>{
+                        throw err;
+                    });
+                }
+                connection.query(flightInvoice, [userId, flightID, departureDate, numPerson], (err, results, fields)=>{
+                    if(err){
+                        connection.rollback(()=>{
+                            throw err;
+                        });
+                    }
+                    connection.commit((err)=>{
+                        if(err){
+                            connection.rollback(()=>{
+                                throw err;
+                            });
+                        }
+                    });
+                });
+            });
+        });
+    });
+};
+
+exports.bookPackage = (req, res) => {
+    const userId = req.body.user_id;
+    const packageId = req.body.package_id;
+    const numPerson = req.body.num_person;
+    const debitCardNum = req.body.card_number;
+    const debitCVV = req.body.cvv;
+    const accountHolderName = req.body.account_holder_name;
+    const date = Date.now();
+    const transactionQuery = 'insert into Transaction (Amount, CardNumber, AccountHolderName, TransactionDate) values (?, ?, ?, ?);';
+    const packageInvoice = 'insert into PackageInvoice(UserID, TravelPackageID, NumPerson, TransactionID) values(?, ?, ?, ?, LAST_INSERT_ID())';
+    const getPerPersonCost = 'select PerPersonCost from TravelPackage where ID = ?';
+    
+    connection.beginTransaction((err)=>{
+        if(err){
+            throw err;
+        }
+        connection.query(getPerPersonCost, [packageId], (err, results, fields) => {
+            if(err){
+                throw err;
+            }
+            const perPersonCost = results[0].PerPersonCost;
+            const amount = numPerson * perPersonCost;
+            connection.query(transactionQuery, [amount, debitCardNum, accountHolderName, date], (err, results, fields)=>{
+                if(err){
+                    connection.rollback(()=>{
+                        throw err;
+                    });
+                }
+                connection.query(packageInvoice, [userId, packageId, numPerson], (err, results, fields)=>{
+                    if(err){
+                        connection.rollback(()=>{
+                            throw err;
+                        });
+                    }
+                    connection.commit((err)=>{
+                        if(err){
+                            connection.rollback(()=>{
+                                throw err;
+                            });
+                        }
+                    });
+                });
+            });
+        });
+    });
+};
 
 exports.addPackage = (req, res) => {
     const numDays = req.body.numDays;
@@ -367,11 +557,87 @@ exports.addPackage = (req, res) => {
     const description = req.body.description;
     const otherCost = req.body.otherCost;
     const discount = req.body.discount;
-    const query = 'insert into TravelPackage(PackageTag, Description, NumDays, NumNights, HotelId, OtherCost, Discount) values (?, ?, ?, ?, ?, ?, ?)';
-    connection.query(query, [packageTag, description, numDays, numNights, hotelId, otherCost, discount], (err, results, fields) => {
+    const departureDate = req.body.departure_date;
+    const query = "insert into TravelPackage(PackageTag, Description, NumDays,"+
+    " NumNights, DepartureDate, HotelId, OtherCost, Discount)"+
+    " values (?, ?, ?, ?, ?, ?, ?, ?)";
+    connection.query(query, [packageTag, description, numDays, numNights, departureDate, hotelId, otherCost, discount], (err, results, fields) => {
+        if(err){
+            res.json({
+                code: 400,
+                message: 'could not get the hotel details'
+            });
+        }
         res.json({
-            err,
-            results
+            code: 200,
+            message: 'successfully added package'
         });
+    });
+};
+
+exports.addPackageHotels = (req, res)=>{
+    const packageId = req.body.package_id;
+    const hotelId = req.body.hotel_id;
+    const dayNum = req.body.day_num;
+
+    const query = "insert into TravelPackageHotels(TravelPackageID, HotelID, DayNum) values (?, ?, ?)";
+    connection.query(query, [packageId, hotelId, dayNum], (err, results, fields)=>{
+        if(err){
+            res.json({
+                code: 400,
+                message: 'adding hotel to package failed'
+            });
+            return;
+        }
+        res.json({
+            code: 200,
+            message: 'successfully added hotel to package'
+        });
+        connection.commit();
+    });
+};
+
+exports.addPackageSightSeeing = (req, res) => {
+    const packageId = req.body.package_id;
+    const placeId = req.body.place_id;
+    const dayNum = req.body.day_num;
+
+    const query = "insert into TravelPackageHotels(TravelPackageID, PlaceID, DayNum) values (?, ?, ?)";
+    connection.query(query, [packageId, placeId, dayNum], (err, results, fields)=>{
+        if(err){
+            res.json({
+                code: 400,
+                message: 'adding hotel to package failed'
+            });
+            return;
+        }
+        res.json({
+            code: 200,
+            message: 'successfully added hotel to package'
+        });
+        connection.commit();
+    });
+};
+
+exports.addPackageTravelInfo = (req, res) =>{
+    const packageId = req.body.package_id;
+    const departureDate = req.body.departure_date;
+    const flightId = req.body.flight_id;
+    const dayNum = req.body.day_num;
+
+    const query = "insert into TravelInfo(DepartureDate, TravelPackageID, FlightID, DayNum) values (?, ?, ?, ?)";
+    connection.query(query, [departureDate, packageId, flightId, dayNum], (err, results, fields)=>{
+        if(err){
+            res.json({
+                code: 400,
+                message: 'adding hotel to package failed'
+            });
+            return;
+        }
+        res.json({
+            code: 200,
+            message: 'successfully added hotel to package'
+        });
+        connection.commit();
     });
 };
